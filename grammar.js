378440fc76f08fc37fcf7cd5556ccab4231abb93
Rémi Labeyrie <calops@tocards.net>
2025-02-19 00:30:10 +0100

    fix: handle trailing comma in formals

--- grammar.js
+++ grammar.js
@@ -126,8 +126,7 @@ module.exports = grammar({
         seq(
           "{",
           commaSep1(field("formal", $.formal)),
-          ",",
-          field("ellipses", $.ellipses),
+          optional(seq(",", optional(field("ellipses", $.ellipses)))),
           "}",
         ),
         seq("{", field("ellipses", $.ellipses), "}"),
