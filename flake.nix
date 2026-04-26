d0172f767439e984b14a64d84e6a8123e902d682
Michael Daniels <mdaniels5757@gmail.com>
2026-04-26 16:40:07 -0400

    ci: use supported x86_64-darwin runner (#170)
    See https://github.blog/changelog/2025-09-19-github-actions-macos-13-runner-image-is-closing-down/

--- flake.nix
+++ flake.nix
@@ -110,7 +110,7 @@
         # Inherit GHA actions matrix from a subset of platforms supported by hosted runners
         platforms = {
           "x86_64-linux" = "nscloud-ubuntu-22.04-amd64-4x16";
-          "x86_64-darwin" = "macos-13";
+          "x86_64-darwin" = "macos-15-intel";
           "aarch64-darwin" = "macos-latest";
           "aarch64-linux" = "nscloud-ubuntu-22.04-arm64-4x16";
         };
